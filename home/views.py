66204be2aae4f834c271a71f16a160a8e1979974
Dillon Mc Caffrey <dmccaffrey01@gmail.com>
2023-09-19 14:04:10 +0100

    Add products views and templates

--- home/views.py
+++ home/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from products.models import Category
 
 def index(request):
     """A view to render the index page"""
-    
+
     return render(request, 'home/index.html')
