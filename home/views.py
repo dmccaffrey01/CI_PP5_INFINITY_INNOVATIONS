5abf01a3b56645ed687e614496a7d66cf23dffc9
Dillon Mc Caffrey <dmccaffrey01@gmail.com>
2023-09-19 13:52:33 +0100

    Add product app, fixutures and load data

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from products.models import Category
 
 def index(request):
     """A view to render the index page"""
-
+    
     return render(request, 'home/index.html')
