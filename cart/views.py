efd4ce9eb40a947e24eb8b5bd74a38ce10279311
Dillon Mc Caffrey <dmccaffrey01@gmail.com>
2023-09-23 14:40:28 +0100

    Finish product theme logic

--- cart/views.py
+++ cart/views.py
@@ -15,12 +15,24 @@ def add_to_cart(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    theme = None
+    if 'product_theme' in request.POST:
+        theme = request.POST['product_theme']
     cart = request.session.get('cart', {})
 
-    if item_id in list(cart.keys()):
-        cart[item_id] += quantity
+    if theme:
+        if item_id in list(cart.keys()):
+            if theme in cart[item_id]['items_by_theme'].keys():
+                cart[item_id]['items_by_theme'][theme] += quantity
+            else:
+                cart[item_id]['items_by_theme'][theme] = quantity
+        else:
+            cart[item_id] = {'items_by_theme': {theme: quantity}}
     else:
-        cart[item_id] = quantity
+        if item_id in list(cart.keys()):
+            cart[item_id] += quantity
+        else:
+            cart[item_id] = quantity
 
     request.session['cart'] = cart
 
