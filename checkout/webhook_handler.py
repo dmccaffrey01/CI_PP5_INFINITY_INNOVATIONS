10bdf2e3a3b3f5b83e201a9b77fba8ac7f147210
Dillon Mc Caffrey <dmccaffrey01@gmail.com>
2023-09-26 10:07:28 +0100

    Update checkout form

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -63,7 +63,7 @@ class StripeWH_Handler:
                     street_address1__iexact=shipping_details.address.line1,
                     street_address2__iexact=shipping_details.address.line2,
                     county__iexact=shipping_details.address.state,
-                    county__iexact=grand_total
+                    grand_total=grand_total,
                     original_cart=cart,
                     stripe_pid=pid,
                 )
