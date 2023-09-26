34636d2bc2672ab67985de875d7e15c53b75768b
Dillon Mc Caffrey <dmccaffrey01@gmail.com>
2023-09-26 17:15:12 +0100

    Troubleshooting

--- infinity_innovations/settings.py
+++ infinity_innovations/settings.py
@@ -27,7 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://ci-pp5-infinity-innovations-75b032391d20.herokuapp.com/', 'localhost', '127.0.0.1']
 
