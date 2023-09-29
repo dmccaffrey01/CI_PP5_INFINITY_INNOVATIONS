2af2983cb843ad47de8f916fbbb5d0e9c3b6d9b8
Dillon Mc Caffrey <dmccaffrey01@gmail.com>
2023-09-29 12:30:03 +0100

    Add contact page

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('policy/', views.policy, name='policy'),
+    path('contact/', views.contact, name='contact'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import ContactMessage
+from .forms import ContactForm
+from django.core.mail import send_mail
+import os
 
 def index(request):
     """A view to render the index page"""
@@ -9,3 +13,29 @@ def policy(request):
     """A view to render the policy page"""
 
     return render(request, 'policy/policy.html')
+
+def contact(request):
+    """A view to render the contact page"""
+    
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            send_contact_email(form.cleaned_data)
+            return redirect('home')
+    else:
+        form = ContactForm()
+    
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'contact/contact.html', context)
+
+
+def send_contact_email(data):
+    subject = 'New Contact Message'
+    message = f"Name: {data['name']}\nEmail: {data['email']}\n\n{data['message']}"
+    from_email = os.environ.get('EMAIL_HOST_USER')
+    to_email = [os.environ.get('EMAIL_HOST_USER')]
+    send_mail(subject, message, from_email, to_email)
