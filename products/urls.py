e017e843953a517c320db168694555cf688399f8
Dillon Mc Caffrey <dmccaffrey01@gmail.com>
2023-09-29 15:49:09 +0100

    Add product admin add product form, view, url, template

--- products/urls.py
+++ products/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.all_products, name='products', kwargs={'universe': 'all'}),
     path('real/', views.all_products, name='real_products', kwargs={'universe': 'real'}),
     path('digital/', views.all_products, name='digital_products', kwargs={'universe': 'digital'}),
-    path('<product_id>', views.product_detail, name='product_detail'),
+    path('<int:product_id>/', views.product_detail, name='product_detail'),
+    path('add/', views.add_product, name='add_product'),
 ]
--- products/views.py
+++ products/views.py
@@ -4,6 +4,7 @@ from django.db.models import Q
 from django.http import HttpResponse
 from django.db.models.functions import Lower
 from .models import Product, Category, Brand
+from .forms import ProductForm
 
 
 def all_products(request, universe):
@@ -90,5 +91,17 @@ def product_detail(request, product_id):
     return render(request, 'products/product_detail.html', context)
 
 
+def add_product(request):
+    """ Add a product to the store """
+
+    form = ProductForm()
+
+    template = 'products/add_product.html'
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
 
 
