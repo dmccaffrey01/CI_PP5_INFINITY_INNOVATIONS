7b6b725f194daca1afda0b2a5d3ade680a4072e6
Dillon Mc Caffrey <dmccaffrey01@gmail.com>
2023-10-11 13:12:30 +0100

    Change styling of reviews

--- home/admin.py
+++ home/admin.py
@@ -1,2 +1,11 @@
 from django.contrib import admin
+from .models import Review
 
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'message',
+    )
+
+admin.site.register(Review, ReviewAdmin)
